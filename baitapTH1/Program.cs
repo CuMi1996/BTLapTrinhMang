c44c0d3435745a9a9dc9e5973ec889d1a688d83e
Lê Anh Tiến <14dh110103@gmail.com>
2017-09-29 11:11:45 +0700

    BTLTM

---
+++ baitapTH1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace baitapTH1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ten may: \"{0}\"", Dns.GetHostName());
+            IPAddress[] myIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress ip in myIP)
+            {
+                Console.WriteLine(" IP {0}: {1}", ip.AddressFamily.ToString(), ip.ToString());
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ baitapTH2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace baitapTH2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ten mien hoac dia chi IP");
+            String host = Console.ReadLine();
+            IPHostEntry ipHE = Dns.GetHostEntry(host);
+            Console.WriteLine("HostName: {0}", ipHE.HostName);
+
+            foreach (string s in ipHE.Aliases)
+            {
+                Console.WriteLine("\t+Alias: {0} \n", s);
+            }
+            foreach (IPAddress i in ipHE.AddressList)
+            {
+                Console.WriteLine("\t+ IP: {0}", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
